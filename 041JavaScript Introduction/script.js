521c070044ba595855a780051790650dae306fc2
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2024-12-12 13:36:52 +0530

    Web_Development

---
+++ 041JavaScript Introduction/script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+console.log("Code is running...")
+console.log("Code is still running...")
+console.log("Code looking wow!")
+
+var a = prompt("Enter your number")
+
+console.log("Your number is " + a)
+
+var isTrue = confirm("Are you want to exit and blast your computer")
+
+if (isTrue) {
+    console.log("Computer is Blasting")
+}
+else {
+    console.log("Computer is not blasting")
+}
