3257e670ed6778f3698f0abffa9a1f426d3b1ae4
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2024-12-13 21:04:58 +0530

    Conditional Statements

---
+++ 043Conditional Statements/index.js
@@ -0,0 +1,12 @@
+console.log("Hello");
+
+let age = 17;
+let grace = 2;
+console.log(age ** grace);
+
+if ((age+grace) > 18) {
+  console.log("You are adult");
+} else {
+  console.log("You are not adult");
+}
+
