19deedb58649bc2572edfe63bb806dd62d372e0b
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2024-12-13 19:16:34 +0530

    Variables in JS

---
+++ 042JS Variables/script.js
@@ -0,0 +1,34 @@
+console.log("Hello")
+
+let a = 5
+let b = 6
+let c = "Sigma"
+
+{
+    let a = 99
+    console.log("a:" + a)
+}
+console.log("a:" + a)
+console.log(a + b)
+console.log(typeof a, typeof b, typeof c)
+
+let x = "hi"
+let y = 22
+let z = 3.5
+const p = true
+let q = undefined
+let r = null
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    subject: "CSE",
+    "subject code" : "3CSE57"
+}
+
+console.log(o)
+o.salary = "100Crores"
+console.log(o)
+o.salary = "500Crores"
+console.log(o)
