1703cafad611d9db00c8bf68ee1f5aff2c6a15dd
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2024-12-14 11:43:39 +0530

    Loops

---
+++ 044Loops/index.js
@@ -0,0 +1,34 @@
+console.log("Hi, Let talk about Loops");
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// let obj = {
+//     "name": "Js",
+//     "role": "lang"
+// }
+
+// for (const key in obj) {
+//     if (Object.prototype.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key,element);
+//     }
+// }
+
+    
+// for (const c of "JavaScript") {
+//     console.log(c);
+// }
+
+// let i = 5;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+let i = 5;
+do {
+    console.log(i);
+    i++;
+} while (i<5);
