589716f3b7cabd1036cb65444695b855d6a08d4d
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2024-12-31 11:11:24 +0530

    Updated

--- 059Hacking Simulator/script.js
+++ 059Hacking Simulator/script.js
@@ -1,25 +1,29 @@
 function delay(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  }
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-  async function main() {
-    let a = document.querySelector(".container");
-    a.innerHTML = ""; // Clear container for each iteration
+async function main() {
+  let a = document.querySelector(".container");
 
-    await delay(1000);
-    a.innerHTML += "Initializing Hacking<span class='blinking-dots'></span><br>";
-    await delay(4000);
-    a.innerHTML += "Reading your files<span class='blinking-dots'></span><br>";
-    await delay(4000);
-    a.innerHTML += "Password files detected<span class='blinking-dots'></span><br>";
-    await delay(4000);
-    a.innerHTML += "Sending all password and personal files to server<span class='blinking-dots'></span><br>";
-    await delay(4000);
-    a.innerHTML += "Cleaning up<span class='blinking-dots'></span><br>";
-  }
+  await delay(1000);
+  a.innerHTML += "Initializing Hacking<span class='blinking-dots'></span><br>";
+  await delay(4000);
 
-  setInterval(() => {
-    main();
-  }, 20000); // Allow time for the process to complete
+  a.innerHTML += "Reading your files<span class='blinking-dots'></span><br>";
+  await delay(4000);
+
+  a.innerHTML += "Password files detected<span class='blinking-dots'></span><br>";
+  await delay(4000);
+
+  a.innerHTML += "Sending all password and personal files to server<span class='blinking-dots'></span><br>";
+  await delay(4000);
+
+  a.innerHTML += "Cleaning up<span class='blinking-dots'></span><br>";
+
+  document.querySelector(".hacked").innerHTML =  `<div>Your Device is Hacked</div>
+  <div>☠️☠️☠️</div>`;
+}
+
+main();
