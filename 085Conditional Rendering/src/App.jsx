9e27bd24d268277aedaa38e85133319e9920779c
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2025-01-24 21:06:59 +0530

    Conditional Rendering

---
+++ 085Conditional Rendering/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [showbtn, setshowbtn] = useState(false)
+  const [todo, settodo] = useState([
+    {
+      title: "Todo 1",
+      desc: "Description 1"
+    },
+    {
+      title: "Todo 2",
+      desc: "Description 2"
+    },
+    {
+      title: "Todo 3",
+      desc: "Description 3"
+    }
+  ])
+  
+  // const Todo = ({todo}) => {
+  //   return (<>
+  //     <div className="m-4 border border-pink-300">
+  //     <div className="todo">{ todo.title}</div>
+  //     <div className="todo">{ todo.desc}</div>
+  //     </div>
+  //   </>)
+  // }
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      {todo.map(todo => {
+        // return <Todo key={todo.title} todo={todo} />;
+        return (<div key={todo.title} className="m-4 border border-pink-300">
+          <div className="todo">{ todo.title}</div>
+          <div className="todo">{ todo.desc}</div>
+          </div>)
+      })}
+      <h1>Vite + React</h1>
+      {/* {showbtn?<button>I will be visible when showbtn = true</button>:""} */}
+      {showbtn&&<button>I will be visible when showbtn = true</button>}
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)
+        }>
+          count is {count}
+        </button>
+        <button onClick={() => setshowbtn(!showbtn)} style={{display:"block",width:"25vw",marginTop:"5px"}}>
+          Toggle ShowBtn
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
