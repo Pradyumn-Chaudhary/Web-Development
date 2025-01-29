8205f152f0ddadcdc572857cbe7cd6147a2c6885
Pradyumn Chaudhary <pradyumnchaudhary6@gmail.com>
2025-01-29 12:53:42 +0530

    Introduction to NextJS

---
+++ 094next-js/app/about/page.js
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+      <div>
+          <Navbar/>
+          Hi,I am Pradyumn Chaudhary.
+          <br />
+          A Web Developer and Competetive Programmar
+    </div>
+  )
+}
+
+export default page
